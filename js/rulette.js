e1445e14dadfc39d36088fb81db5a9831385d36f
Esteban Coria <esteban.coria@despegar.com>
2017-12-12 21:59:13 -0300

    Stable version with beutifull background

--- js/rulette.js
+++ js/rulette.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
      });
 });
 
-var options2 = [];
+var options = [];
+var arc;
 
 function parseCSV(data){
 
@@ -28,16 +29,16 @@ function parseCSV(data){
     var oldArray = results.data;
 
     for (var i = 1; i < oldArray.length -1; i++) {
-      options2.push(oldArray[i][2] + " " + oldArray[i][3]);
+      options.push((oldArray[i][2] + " " + oldArray[i][3]).toString());
     }
   }
+  arc = Math.PI / (options.length / 2);
   drawRouletteWheel();
 }
 
-var options = ["Marcos Molineri", "Patricia Harkes", "Yesica Orellana", "Agustina Lanari", "Agustina Lopez", "Rosana Inchaurrondo", "Alexia Navarrete", "Cecilia Datko", "Raul Moviglia", "Andrea Cruz", "Melisa Mainero", "Sofia Guerrini", "INDIRA VEGA", "Sofia Aloise", "Catalina Carlos", "Julieta Gobbi", "Thalía Hurtado", "juan alvarez lojo", "Victoria Villalobos Sambucaro", "Julian Mosquera", "Lucas Alonso", "Teresita Bulfon", "María José Calderaro", "Anahi Castellano", "Eric Collard Bovy", "Matias Ferreyra", "Mariana Galeano", "Gonzalo Luna", "Diego Corrao", "Paola Giannattasio", "Marina Leon", "Magali Ortiz Miller", "Agustina Pared", "Paula Pejkovic", "Javier Peña", "Juan Martin Pietraroia", "Daniel Rozadilla", "Julia Saborido", "Sofia Spoltore", "Catalina Velarde", "Andres Vicente", "Juliana Vitale", "Catalina Guerrero", "Ismael Martinez", "Pablo Hernán Amarilla"];
+// var options = ["Marcos Molineri", "Patricia Harkes", "Yesica Orellana", "Agustina Lanari", "Agustina Lopez", "Rosana Inchaurrondo", "Alexia Navarrete", "Cecilia Datko", "Raul Moviglia", "Andrea Cruz", "Melisa Mainero", "Sofia Guerrini", "INDIRA VEGA", "Sofia Aloise", "Catalina Carlos", "Julieta Gobbi", "Thalía Hurtado", "juan alvarez lojo", "Victoria Villalobos Sambucaro", "Julian Mosquera", "Lucas Alonso", "Teresita Bulfon", "María José Calderaro", "Anahi Castellano", "Eric Collard Bovy", "Matias Ferreyra", "Mariana Galeano", "Gonzalo Luna", "Diego Corrao", "Paola Giannattasio", "Marina Leon", "Magali Ortiz Miller", "Agustina Pared", "Paula Pejkovic", "Javier Peña", "Juan Martin Pietraroia", "Daniel Rozadilla", "Julia Saborido", "Sofia Spoltore", "Catalina Velarde", "Andres Vicente", "Juliana Vitale", "Catalina Guerrero", "Ismael Martinez", "Pablo Hernán Amarilla"];
 
 var startAngle = 0;
-var arc = Math.PI / (options.length / 2);
 var spinTimeout = null;
 
 var spinArcStart = 10;
@@ -79,7 +80,7 @@ function drawRouletteWheel() {
 
     ctx = canvas.getContext("2d");
     // ctx.translate(100,100);
-    ctx.clearRect(100,100,400,400);
+    ctx.clearRect(0,0,800,800);
 
     ctx.strokeStyle = "transparent";
     ctx.lineWidth = 0;
@@ -89,7 +90,7 @@ function drawRouletteWheel() {
     for(var i = 0; i < options.length; i++) {
       var angle = startAngle + i * arc;
       //ctx.fillStyle = colors[i];
-      ctx.fillStyle = getColor(i, options.length);
+      ctx.fillStyle = "rgba(255, 255, 255, 0)"; //getColor(i, options.length);
 
       ctx.beginPath();
       ctx.arc(350, 350, outsideRadius, angle, angle + arc, false);
@@ -106,35 +107,48 @@ function drawRouletteWheel() {
       ctx.translate(350 + Math.cos(angle + arc / 2) * textRadius,
                     350 + Math.sin(angle + arc / 2) * textRadius);
       ctx.rotate(angle + arc / 2 + Math.PI * 2);
+      ctx.rotate(180 * Math.PI / 180);
       var text = options[i];
       ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
       ctx.restore();
     }
 
     //Arrow
+    ctx.save();
     ctx.fillStyle = "black";
     ctx.beginPath();
-    ctx.moveTo(350 - 4, 350 - (outsideRadius + 5));
-    ctx.lineTo(350 + 4, 350 - (outsideRadius + 5));
-    ctx.lineTo(350 + 4, 350 - (outsideRadius - 5));
-    ctx.lineTo(350 + 9, 350 - (outsideRadius - 5));
-    ctx.lineTo(350 + 0, 350 - (outsideRadius - 13));
-    ctx.lineTo(350 - 9, 350 - (outsideRadius - 5));
-    ctx.lineTo(350 - 4, 350 - (outsideRadius - 5));
-    ctx.lineTo(350 - 4, 350 - (outsideRadius + 5));
+    // ctx.moveTo(10 - 4, 650 - (outsideRadius + 5));
+    // ctx.lineTo(10 + 4, 650 - (outsideRadius + 5));
+    // ctx.lineTo(10 + 4, 650 - (outsideRadius - 5));
+    // ctx.lineTo(10 + 9, 650 - (outsideRadius - 5));
+    // ctx.lineTo(10 + 0, 650 - (outsideRadius - 13));
+    // ctx.lineTo(10 - 9, 650 - (outsideRadius - 5));
+    // ctx.lineTo(10 - 4, 650 - (outsideRadius - 5));
+    // ctx.lineTo(10 - 4, 650 - (outsideRadius + 5));
+
+    ctx.moveTo(outsideRadius + insideRadius - (outsideRadius + 5), outsideRadius + insideRadius - 4);
+    ctx.lineTo(outsideRadius + insideRadius - (outsideRadius + 5), outsideRadius + insideRadius + 4);
+    ctx.lineTo(outsideRadius + insideRadius - (outsideRadius - 5), outsideRadius + insideRadius + 4);
+    ctx.lineTo(outsideRadius + insideRadius - (outsideRadius - 5), outsideRadius + insideRadius + 9);
+    ctx.lineTo(outsideRadius + insideRadius - (outsideRadius - 13),outsideRadius + insideRadius + 0);
+    ctx.lineTo(outsideRadius + insideRadius - (outsideRadius - 5), outsideRadius + insideRadius - 9);
+    ctx.lineTo(outsideRadius + insideRadius - (outsideRadius - 5), outsideRadius + insideRadius - 4);
+    ctx.lineTo(outsideRadius + insideRadius - (outsideRadius + 5), outsideRadius + insideRadius - 4);
     ctx.fill();
+
+
   }
 }
 
 function spin() {
   spinAngleStart = Math.random() * 10 + 10;
   spinTime = 0;
-  spinTimeTotal = Math.random() * 3 + 4 * 1000;
+  spinTimeTotal = Math.random() * 5 + 4 * 1000;
   rotateWheel();
 }
 
 function rotateWheel() {
-  spinTime += 30;
+  spinTime += 50;
   if(spinTime >= spinTimeTotal) {
     stopRotateWheel();
     return;
@@ -142,17 +156,18 @@ function rotateWheel() {
   var spinAngle = spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
   startAngle += (spinAngle * Math.PI / 180);
   drawRouletteWheel();
-  spinTimeout = setTimeout('rotateWheel()', 30);
+  spinTimeout = setTimeout('rotateWheel()', 50);
 }
 
 function stopRotateWheel() {
   clearTimeout(spinTimeout);
-  var degrees = startAngle * 180 / Math.PI + 90;
+  var degrees = startAngle * 180 / Math.PI + 180;
   var arcd = arc * 180 / Math.PI;
   var index = Math.floor((360 - degrees % 360) / arcd);
   ctx.save();
   ctx.font = 'bold 30px Helvetica, Arial';
   var text = options[index]
+  document.getElementById("winner-name").innerText = text;
   ctx.fillText(text, 350 - ctx.measureText(text).width / 2, 350 + 10);
   ctx.fillText(text, 600 , 350);
   ctx.restore();
